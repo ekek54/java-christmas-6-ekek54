44a3b8b3807d87814aa3fe3606d0018a61783087
김강년 <64246800+ekek54@users.noreply.github.com>
2023-11-11 13:53:26 +0900

    refactor: OutputView 수정
    static 메서드 인스턴스 메서드로 수정

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class OutputView {
     private static final String ORDER_FORMAT = "%s %d개";
 
-    public static void printOrders(List<OrderDTO> orders) {
+    public void printOrders(List<OrderDTO> orders) {
         System.out.println("<주문 메뉴>");
-        orders.forEach(OutputView::printOrder);
+        orders.forEach(this::printOrder);
     }
 
-    private static void printOrder(OrderDTO orderDTO) {
+    private void printOrder(OrderDTO orderDTO) {
         System.out.println(String.format(ORDER_FORMAT, orderDTO.getMenu(), orderDTO.getCount()));
     }
 }
