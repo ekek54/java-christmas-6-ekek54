93e28d995f87d0657fa9cb91012abdab5db2a97b
김강년 <64246800+ekek54@users.noreply.github.com>
2023-11-13 15:58:15 +0900

    test: equals메서드 calculatePrice 메서드 테스트 추가

--- src/test/java/christmas/domain/order/MenuTest.java
+++ src/test/java/christmas/domain/order/MenuTest.java
@@ -31,4 +31,47 @@ class MenuTest {
         assertThat(soup).isNotNull();
     }
 
+    @Test
+    @DisplayName("메뉴의 이름을 조회하면 메뉴의 이름이 반환된다.")
+    void getName() {
+        //given
+        String menu = "양송이수프";
+        //when
+        Menu soup = Menu.of(menu);
+        //then
+        assertThat(soup.getName()).isEqualTo(menu);
+    }
+
+    @Test
+    @DisplayName("메뉴 이름이 다르면 동등한 메뉴가 아니다.")
+    void equals() {
+        //given
+        String menu = "양송이수프";
+        //when
+        Menu soup = Menu.of(menu);
+        //then
+        assertThat(soup.equals(Menu.of("티본스테이크"))).isFalse();
+    }
+
+    @Test
+    @DisplayName("다른 인스턴스 객체와 비교시 동등하지 않다.")
+    void equalsWithDifferentInstance() {
+        //given
+        String menu = "양송이수프";
+        //when
+        Menu soup = Menu.of(menu);
+        //then
+        assertThat(soup.equals(new Object())).isFalse();
+    }
+
+    @Test
+    @DisplayName("메뉴가 n개일 때의 가격을 계산한다")
+    void calculatePrice() {
+        //given
+        String menu = "양송이수프";
+        //when
+        Menu soup = Menu.of(menu);
+        //then
+        assertThat(soup.calculatePrice(3)).isEqualTo(18000);
+    }
 }
