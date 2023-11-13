4a1b54d95c59a8dff2ddc9530ead4943d18506e3
김강년 <64246800+ekek54@users.noreply.github.com>
2023-11-14 02:41:02 +0900

    feat: 프로그램 구성

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,39 @@
 package christmas;
 
+import christmas.controller.Controller;
+import christmas.controller.ControllerBuilder;
+import christmas.domain.event.ChristmasEvent;
+import christmas.domain.event.GiftEvent;
+import christmas.domain.event.SpecialEvent;
+import christmas.domain.event.WeekdayEvent;
+import christmas.domain.event.WeekendEvent;
+import christmas.domain.order.Orders;
+import christmas.service.EventService;
+import christmas.util.InputErrorHandler;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import christmas.vo.VisitDate;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        InputErrorHandler<VisitDate> visitDateInputErrorHandler = new InputErrorHandler<>("유효하지 않은 날짜입니다.");
+        InputErrorHandler<Orders> ordersInputErrorHandler = new InputErrorHandler<>("유효하지 않은 주문입니다.");
+        EventService eventService = new EventService(List.of(
+                new ChristmasEvent(),
+                new GiftEvent(),
+                new SpecialEvent(),
+                new WeekdayEvent(),
+                new WeekendEvent()
+        ));
+        Controller controller = new ControllerBuilder().setInputView(inputView)
+                .setOutputView(outputView)
+                .setEventService(eventService)
+                .setVisitDateHandler(visitDateInputErrorHandler)
+                .setOrdersHandler(ordersInputErrorHandler)
+                .createController();
+        controller.run();
     }
 }
